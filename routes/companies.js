cae4e026fa1d235c70243aab281648a6c6dc39eb
aleksandra <oleksandra.klevchuk@gmail.com>
2021-10-11 19:20:07 -0500

    update slugify feature.

--- routes/companies.js
+++ routes/companies.js
@@ -28,13 +28,13 @@ router.get('/:code', async(req, res, next) => {
 
 router.post('/', async(req, res, next) => {
     try {
-        let { code, name, description } = req.body;
-        let codeS = slugify(code, { lower: true, trim: true, remove: /[*+~.?()'"!:@]/g });
+        let { name, description } = req.body;
+        let code = slugify(name, { lower: true });
 
         const results = await db.query(
             `INSERT INTO companies (code, name, description) 
              VALUES ($1, $2, $3)
-             RETURNING code, name, description`, [codeS, name, description]
+             RETURNING code, name, description`, [code, name, description]
         );
 
         return res.status(201).json({ company: results.rows[0] });
